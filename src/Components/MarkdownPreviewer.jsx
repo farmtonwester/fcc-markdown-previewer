c49f242019273ae7e33ac11f131a513791993a10
Weston Farmer <wesfarmer11@gmail.com>
2024-04-02 17:10:04 -0500

    markdown previewer component initialized but not yet rendered to screen

---
+++ src/Components/MarkdownPreviewer.jsx
@@ -0,0 +1,30 @@
+import { React, useState } from 'react';
+
+
+
+// class MarkdownPreviewer extends Component {
+//     constructor(props) {
+//         super(props);
+
+//         const [previewText, setPreviewText] = useState(null);
+//     }
+//     render() {
+//         return <>
+//             <textarea id="editor"></textarea>
+//             <div id="preview"></div>
+//         </>
+//     }
+// }
+
+const MarkdownPreviewer= (() =>  (
+    <>
+        <textarea id="editor"></textarea>
+        <div id="preview"></div>"
+        
+    </>
+    
+
+    
+));
+
+export default MarkdownPreviewer
